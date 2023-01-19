80c2c903649742a53d7739783a55eb87b373bbf5
lucasheck <lucasheck23@gmail.com>
2023-01-18 23:42:17 -0400

    1 - Fixing the favicon 2 - Creating HabitDay Component 3 - Creating Header Component 4 - Creating NewHabitForm Component 5 - Creating ProgressBar Component 6 - Creating Summary Component

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,10 +1,60 @@
-// interface HabitProps {
-// 	completed: number;
-// }
+import * as PopOver from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgressBar";
+import clsx from "clsx";
 
-const HabitDay = () => {
+interface HabitDayProps {
+	amount: number;
+	completed: number;
+}
+
+const HabitDay = ({ completed, amount }: HabitDayProps) => {
+	const completedPercentage = Math.round((completed / amount) * 100);
+
 	return (
-		<div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg cursor-pointer"></div>
+		<PopOver.Root>
+			<PopOver.Trigger
+				className={clsx(
+					"w-10 h-10 border-2 rounded-lg cursor-pointer",
+					{
+						"bg-zinc-900 border-zinc-800":
+							completedPercentage === 0,
+						"bg-violet-900 border-violet-700":
+							0 < completedPercentage &&
+							completedPercentage <= 20,
+						"bg-violet-800 border-violet-600":
+							20 < completedPercentage &&
+							completedPercentage <= 40,
+						"bg-violet-700 border-violet-500":
+							40 < completedPercentage &&
+							completedPercentage <= 60,
+						"bg-violet-600 border-violet-400":
+							60 < completedPercentage &&
+							completedPercentage <= 80,
+						"bg-violet-500 border-violet-300":
+							completedPercentage > 80,
+					}
+				)}
+			/>
+
+			<PopOver.Portal>
+				<PopOver.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+					<span className="font-semibold text-zinc-400">
+						Quarta-Feira
+					</span>
+					<span className="mt-1 font-extrabold leading-tight text-3xl">
+						18/01
+					</span>
+
+					<ProgressBar progress={75} />
+
+					<PopOver.Arrow
+						height={8}
+						width={16}
+						className="fill-zinc-900"
+					/>
+				</PopOver.Content>
+			</PopOver.Portal>
+		</PopOver.Root>
 	);
 };
 
